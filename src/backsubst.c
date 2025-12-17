e76a9df433c22b943297a139255218d26e2dd028
aleksasol <01206826@pw.edu.pl>
2025-12-17 23:53:39 +0100

    Dodalam podstawienie wsteczne

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,39 @@
 #include "backsubst.h"
+
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix* x, Matrix* mat, Matrix* b) {
+    // Sprawdzenie poprawności wymiarów:
+    // Macierz główna musi być kwadratowa, a wektory b i x muszą mieć tyle samo wierszy.
+    if (mat->r != mat->c || mat->r != b->r || x->r != b->r) {
+        return 2;
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    int i, j;
+    int n = mat->r;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    // Główna pętla: iterujemy od ostatniego wiersza do pierwszego
+    for (i = n - 1; i >= 0; i--) {
 
-				return 0;
+        // Zabezpieczenie przed dzieleniem przez zero
+        if (mat->data[i][i] == 0) {
+            return 1;
+        }
+
+        double s = 0;
+
+        // Sumowanie wyrazów, które już obliczyliśmy (stojących na prawo od przekątnej)
+        for (j = i + 1; j < n; j++) {
+            s += mat->data[i][j] * x->data[j][0];
+        }
+
+        // Obliczenie końcowej wartości x[i]
+        x->data[i][0] = (b->data[i][0] - s) / mat->data[i][i];
+    }
+
+    return 0;
 }
-
 
