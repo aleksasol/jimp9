b08f3a6f12cfaa3eed1a2a2fd524b721159617e6
aleksasol <01206826@pw.edu.pl>
2026-01-07 20:01:13 +0100

    Update gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -22,7 +22,7 @@ int eliminate(Matrix *mat, Matrix *b){
         }
 
         // Sprawdzenie czy macierz nie jest osobliwa
-        if (fabs(mat->data[max][k]) < 1e-8) {
+        if (fabs(mat->data[max][k]) < 1e-15) {
             return 1;
         }
 
